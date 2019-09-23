8950a6e30c9920b9d0a5ff1406c45ed471b453f3
Panagiotis Bakas <pan22bak@gmail.com>
2019-09-23 16:48:36 +0300

    Update downloadPinterestBoardImages.py

--- downloadPinterestBoardImages.py
+++ downloadPinterestBoardImages.py
@@ -1,5 +1,3 @@
-    import pprint
-    import requests
     import urllib
     import re
     import time
@@ -15,7 +13,8 @@
     time.sleep(2)
     lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     match=False
-    while(match==False):
+    limit=7 #limit of scrolls
+    while(match==False and limit>0): #auto scroll till end or till limit
         lastCount = lenOfPage
         time.sleep(3)
         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
@@ -27,7 +26,6 @@
 
     response = browser.page_source.encode(encoding='UTF-8')
 
-    toDel =[]
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
 
     print(len(urls))
@@ -37,18 +35,18 @@
         else:
             urls[i]= ""
 
-    urls = list(set(urls))
+    urls = list(set(urls))#remove duplicates
 
-    urls = list(filter(None, urls)) # fastest
-    urls = list(filter(bool, urls)) # fastest
-    urls = list(filter(len, urls))  # a bit slower
+    urls = list(filter(None, urls)) 
+    urls = list(filter(bool, urls))
+    urls = list(filter(len, urls)) 
 
-    with open('raw.txt', 'w') as f:
+    with open('raw.txt', 'w') as f: #writes the image links in a txt file
         for url in urls:
             f.write("%s\n" % url)
     f.close()
 
-    for i in range(len(urls)):
+    for i in range(len(urls)):#save images 
         urllib.urlretrieve(urls[i], "images/"+str(i)+"-image.jpg")
 
 
