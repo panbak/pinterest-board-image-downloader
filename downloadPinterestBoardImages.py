7a5726491bf50f719419c0009b5e7a12216f6bf1
Panagiotis Bakas <pan22bak@gmail.com>
2019-11-06 19:38:32 +0200

    Update downloadPinterestBoardImages.py
    changes for python 3, catch 403:Forbidden error

--- downloadPinterestBoardImages.py
+++ downloadPinterestBoardImages.py
@@ -1,13 +1,14 @@
-    import urllib
-    import re
-    import time
+# -*- coding: utf-8 -*-
+import urllib.request
+import re
+import time
 
-    from selenium import webdriver
+from selenium import webdriver
 
-
+def downloadPinterestImages():
     browser = webdriver.Firefox(executable_path=r'geckodriver.exe')
 
-    link="pinterest board lik" #the pinterest board link
+    link="https://gr.pinterest.com/pin/288934132334754326/"
     browser.get(link)
 
     time.sleep(2)
@@ -18,14 +19,16 @@
         lastCount = lenOfPage
         time.sleep(3)
         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        limit = limit-1;
         if lastCount==lenOfPage:
             print("stop scrolling")
             match=True
         else:
             print("scrolling..")
 
-    response = browser.page_source.encode(encoding='UTF-8')
+    response = browser.page_source#.encode(encoding='UTF-8')
 
+    toDel =[]
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
 
     print(len(urls))
@@ -35,18 +38,23 @@
         else:
             urls[i]= ""
 
-    urls = list(set(urls))#remove duplicates
+    urls = list(set(urls))
 
-    urls = list(filter(None, urls)) 
-    urls = list(filter(bool, urls))
-    urls = list(filter(len, urls)) 
+    urls = list(filter(None, urls)) # fastest
+    urls = list(filter(bool, urls)) # fastest
+    urls = list(filter(len, urls))  # a bit slower
 
-    with open('raw.txt', 'w') as f: #writes the image links in a txt file
+    with open('raw.txt', 'w') as f:
         for url in urls:
             f.write("%s\n" % url)
     f.close()
 
-    for i in range(len(urls)):#save images 
-        urllib.urlretrieve(urls[i], "images/"+str(i)+"-image.jpg")
+    for i in range(len(urls)):
+        try:
+            urllib.request.urlretrieve(urls[i], "images/"+str(i)+"-image.jpg")
+        except:
+            print("Broken Link") 
+        
+        
 
-
+downloadPinterestImages()
